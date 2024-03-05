8065ad0cedb1e95bd0d2ce73cf366311c42a7bf0
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-03-05 14:44:16 -0300

    Criação do projeto + Header sujeito a alterações

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import headerSMT from '@/components/headerSMT.vue'
+</script>
+<template>
+  <headerSMT/>
+</template>
---
+++ src/views/HomeSMT.vue
@@ -0,0 +1,12 @@
+<script setup>
+import headerSMT from '../components/headerSMT.vue'
+</script>
+
+<template>
+  <header>
+    <headerSMT />
+  </header>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
