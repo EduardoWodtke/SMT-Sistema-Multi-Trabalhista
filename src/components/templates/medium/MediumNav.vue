b0bbde56a6af9092e54ea1feda40f8a9045371dd
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-08-13 14:52:59 -0300

    criação do nav medium

---
+++ src/components/templates/medium/MediumNav.vue
@@ -0,0 +1,53 @@
+  <script setup>
+// import { useRouter } from 'vue-router';
+
+// const router = useRouter()
+</script>
+<template>
+  <nav>
+    <div class="info">
+      <ul>
+        <router-link to="/" class= "underline">
+          <li>Ranking</li>
+        </router-link>
+        <router-link to="/" class="underline">
+          <li>Bate-papo</li>
+        </router-link>
+        <router-link to="/" class="underline">
+          <li>Trabalhadores</li>
+        </router-link>
+        <router-link to="/" class="underline">
+          <li>Histórico</li>
+        </router-link>
+        <router-link to="/" class="underline">
+          <li>Favoritos</li>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+<style scoped>
+nav{
+  background-color: black;
+  display: flex;
+  justify-content: space-around;
+}
+
+.underline {
+  text-decoration: none;
+}
+.mdi {
+  font-size: 5vh;
+}
+
+ul {
+  display: flex;
+  flex-direction: row;
+}
+
+li {
+  text-align: center;
+  padding: 0 5vh;
+  list-style: none;
+}
+</style>
