05a9e35d96791786876574deb40620553c09709e
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-08-19 19:18:33 -0300

    esqueleto da pagina trabalhadores

--- src/views/TrabalhadoresSMT.vue
+++ src/views/TrabalhadoresSMT.vue
@@ -1,2 +1,42 @@
 <script setup></script>
-<template></template>
+<template>
+    <div id="container">
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+        <div class="trabalhador"><div class="bolinha">IMG</div><p>Nome</p></div>
+    </div>
+</template>
+<style scoped>
+#container{
+    display: grid;
+    grid-template-columns: auto auto auto;
+    align-items: center;
+    margin: 5vh;
+}
+.trabalhador{
+    background-color: blueviolet;
+    width: 54vh;
+    font-size: 5vh;
+    text-align: center;
+    margin: 3vh;
+    height: 20vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-right: 5vh;
+    border-bottom-left-radius: 10vh;
+    border-top-left-radius: 10vh;
+}
+.bolinha{
+    background-color: yellow;
+    border: 3px solid black;
+    width: 20vh;
+    border-radius: 20vh;
+}
+</style>
