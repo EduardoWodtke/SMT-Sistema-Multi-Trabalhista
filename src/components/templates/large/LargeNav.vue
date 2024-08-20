b2300e80209665044a95c00bee9118ff489afd85
MeDudaSilva <mariaeduarda.11.me4@gmail.com>
2024-08-20 13:09:46 -0300

    hambuguer  lindo maravilhoso cria d+

--- src/components/templates/large/LargeNav.vue
+++ src/components/templates/large/LargeNav.vue
@@ -11,31 +11,41 @@ const clickHamburguer = () => {
 </script>
 <template>
   <nav>
-    <div id="info" :class="servicos" ? >
-      <div v-if="servicos" @click="clickHamburguer()">
-        <button class="butao mdi mdi-alpha-x"></button>
+    <div id="info" :class="servicos">
+      <div v-if="servicos" class="hamburguer">
+        <p>Limpador de vidro</p>
+        <p>Engenheiro</p>
+        <p>Encanador</p>
+        <p>Eletricista</p>
+        <p>Jardineiro</p>
+        <p>Arquiteto</p>
+        <p>Pedreiro</p>
+        <p>Diarista</p>
+        <p>Pintor</p>
+        <p>Baba</p>
+        <div>
+          <button class="butao mdi mdi-alpha-x" @click="clickHamburguer()"></button>
+        </div>
       </div>
-      <Menu v-else @click="clickHamburguer()" class="mdi mdi-menu" size="5vh"/>
+      <Menu @click="clickHamburguer()" class="mdi mdi-menu" size="5vh" />
       <ul>
-        <!-- <template v-if> -->
-          <router-link to="Ranking" class="underline">
-            <li>Ranking</li>
-          </router-link>
-          <router-link to="Chat" class="underline">
-            <li>Bate-papo</li>
-          </router-link>
-          <router-link to="Trabalhadores" class="underline">
-            <li>Trabalhadores</li>
-          </router-link>
-          <router-link to="Historico" class="underline">
-            <li>Histórico</li>
-          </router-link>
-          <router-link to="Favoritos" class="underline">
-            <li>Favoritos</li>
-          </router-link>
-        <!-- </template> -->
+        <router-link to="Ranking" class="underline">
+          <li>Ranking</li>
+        </router-link>
+        <router-link to="Chat" class="underline">
+          <li>Conversas</li>
+        </router-link>
+        <router-link to="Trabalhadores" class="underline">
+          <li>Trabalhadores</li>
+        </router-link>
+        <router-link to="Historico" class="underline">
+          <li>Histórico</li>
+        </router-link>
+        <router-link to="Favoritos" class="underline">
+          <li>Favoritos</li>
+        </router-link>
       </ul>
-      
+
       <!-- <template v-else="isPrestador">
         <button  @click="isPrestador = !isPrestador"></button>
         <router-link to="/" class="underline">
@@ -58,6 +68,30 @@ const clickHamburguer = () => {
   </nav>
 </template>
 <style scoped>
+p{
+  text-shadow: gray 3px 3px 5px;
+}
+.butao{
+  display: flex;
+  font-size: 5vh;
+  position: relative;
+  background: none;
+  border: none;
+  left: 20vh;
+  bottom: 1vh;
+}
+.hamburguer{
+  display: block;
+  background-color: #00173d;
+  position: absolute;
+  padding: 2vh 0 0 3vh;
+  width: 15%;
+  line-height: 6vh;
+  bottom: 21%;
+  border: solid black 3px;
+  font-size: 3vh;
+  box-shadow: rgb(29, 29, 29) 3px 4px 10px;
+}
 .underline {
   text-decoration: none;
 }
