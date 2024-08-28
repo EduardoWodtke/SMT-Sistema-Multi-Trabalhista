fab685697a185423282fda83281195130f39a3a2
EduardoWodtke <eduardorwodtkeifc@gmail.com>
2024-08-28 16:18:59 -0300

    Prepraparando o site para introdução do logout

--- src/views/HomeSMT.vue
+++ src/views/HomeSMT.vue
@@ -1,4 +1,29 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+import { PassageUser } from '@passageidentity/passage-elements/passage-user';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+const getUserInfo = async () => {
+  try {
+    const authToken = localStorage.getItem('psg_auth_token');
+    const passageUser = new PassageUser(authToken);
+    const user = await passageUser.userInfo(authToken);
+    if (user) {
+      await authStore.setToken(authToken);
+    } else {
+      authStore.unsetToken();
+    }
+  } catch (error) {
+    authStore.unsetToken();
+  }
+};
+
+onMounted(() => {
+  getUserInfo();
+});
+</script>
 <template>
   <div class="slide">
     <img src="@/assets/trabalhadores.jpeg" alt="" />
