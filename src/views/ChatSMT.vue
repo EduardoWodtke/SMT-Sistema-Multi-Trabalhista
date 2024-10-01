7d537ede835d12c8ac6ea684b02448829d9a12b3
VictorSedlak1 <victorsedlak04@gmail.com>
2024-10-01 16:53:23 -0300

    Commit do Vitin

--- src/views/ChatSMT.vue
+++ src/views/ChatSMT.vue
@@ -2,14 +2,16 @@
 <template>
   <div id="container">
     <div class="tabela-container">
-      <p>AMIGOS</p>
-        <div class="tabela">
-            
-        </div>
+      <p>CONVERSAS</p>
+      <div class="tabela">
+
+      </div>
     </div>
     <div class="chat">
-      <div class="demonio"> <div class="bolinha">IMG</div>  <h3>Fulano</h3></div>
-     
+      <div class="perfil">
+        <div class="bolinha">IMG</div>
+        <h3>Fulano</h3>
+      </div>
       <div class="conversar">
         <input size="50" type="text" v-model="chat" id="conversar" placeholder="Digitar..." />
       </div>
@@ -17,89 +19,119 @@
   </div>
 </template>
 <style>
+.chat {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: 45vh;
+  position: absolute;
+}
+
 #conversar {
-  width: 45vh;
+  width: 70vh;
   height: 30px;
   font-size: 20px;
   margin: 33% 10vh 0 0;
   text-align: center;
   border: 3px solid;
   color: rgb(44, 44, 44);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  margin-top: 50vh;
+  margin-right: 0vh;
+  border-radius: 10px;
+
+
 }
+
 #container {
   display: flex;
   flex-direction: row;
 }
-.historico{
+
+.historico {
   background-color: rgb(221, 221, 221);
   box-shadow: 0.5vh 2vh 3vh 0.5vh black;
   height: 70vh;
   width: 50vh;
   display: none;
-  
-  
+
+
+}
+
+.perfil h3{
+  justify-content: center;
+  align-items: center;
+  margin-right: 80vh;
+  margin-top: 2vh;
 }
 
 
 
-.demonio{
+.perfil {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: space-between;
   background-color: gray;
-  width: 170vh;
-  height:5vh;
+  height: 10vh;
+  width: 100vh;
   text-align: center;
   font-size: 30px;
   border-radius: 10px;
+  text-align: center;
+  /* justify-content: center; */
 
 
-  
+
 }
 
 
 .tabela-container {
-            height:  70vh;
-            width: 50px; /* Largura inicial da tabela */
-            overflow: hidden; /* Esconde o conteúdo que não cabe */
-            position: relative;
-            transition: width 0.3s; /* Suaviza a transição */
-            background-color: #00173d;
-            border-radius: 5px;
-            text-align: center;
-        }
+  height: 70vh;
+  width: 50px;
+  overflow: hidden;
+  position: relative;
+  transition: width 0.3s;
+  background-color: #00173d;
+  border-radius: 5px;
+  text-align: center;
+  box-shadow: ;
+}
 
-        .tabela {
-            width: 100%; /* Largura da tabela */
-            background-color: #2980b9;
-            transition: all 0.3s ease; /* Suaviza a transição da tabela */
-        }
+.tabela {
+  width: 100%;
+  background-color: #2980b9;
+  transition: all 0.3s ease;
+}
 
-        .tabela-container:hover {
-            /* Largura da tabela ao passar o mouse */
-            width: 40vh;
-            height: 70vh;
-        }
+.tabela-container:hover {
+  width: 40vh;
+  height: 70vh;
+}
 
-        .tabela-container:hover .tabela {
-            transform: translateX(-100%); /* Move a tabela para a esquerda */
-        }
+.tabela-container:hover .tabela {
+  transform: translateX(-100%);
+}
 
-        .conteudo {
-            padding: 10px;
-            color: black;
-            /* Começa invisível */
-            transition: opacity 0.3s ease; /* Suaviza a transição da opacidade */
-        }
+.conteudo {
+  padding: 10px;
+  color: black;
+  transition: opacity 0.3s ease;
+}
 
-        .tabela-container:hover .conteudo {
-            opacity: 1; /* Torna o conteúdo visível ao passar o mouse */
-        }
+.tabela-container:hover .conteudo {
+  opacity: 1;
+}
 
 .bolinha {
   background-color: rgb(0, 153, 255);
   border: 1px solid black;
-  width: 4vh;
-  height: 4vh;
+  width: 10vh;
+  height: 10vh;
   border-radius: 20vh;
 }
 
@@ -109,7 +141,7 @@ h4 {
   text-align: center;
   color: rgb(255, 255, 255);
   background-color: #00173d;
-    
+
 }
 
 
@@ -120,36 +152,41 @@ h4 {
     height: 3vh;
     font-size: 2vh;
   }
-  h3{
+
+  h3 {
     margin-left: 13vh;
   }
-  h4{
+
+  h4 {
     font-size: 2.5vh;
   }
 }
+
 @media screen and (max-width: 430px) {
-  #container{
+  #container {
     display: flex;
     flex-direction: column;
     margin: 0;
     align-items: center;
   }
-  #conversar{
+
+  #conversar {
     width: 30vh;
     margin: 150% 0 0 0;
   }
-  h3{
+
+  h3 {
     margin: 0;
     padding: 0;
   }
-  .historico{
+
+  .historico {
     height: 0vh;
     box-shadow: none;
   }
-  h4{
+
+  h4 {
     font-size: 0;
   }
 }
-
-
 </style>
